fb1fd42a962b4a02d95a2fd90bf1cc3caba62dc5
Tim Ebert <tim.ebert@sap.com>
2020-12-03 09:39:31 +0100

    Inject effective version into binary

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -14,5 +14,52 @@
 
 package version
 
-// Version is a string that is overwritten during build time using ld flags.
-var Version = "binary not built correctly"
+import (
+	"fmt"
+	"runtime"
+	"strings"
+
+	apimachineryversion "k8s.io/apimachinery/pkg/version"
+)
+
+var (
+	gitVersion   = "v0.0.0-dev"
+	gitCommit    string
+	gitTreeState string
+	buildDate    = "1970-01-01T00:00:00Z"
+
+	version *apimachineryversion.Info
+)
+
+// Get returns the overall codebase version. It's for detecting
+// what code a binary was built from.
+// These variables typically come from -ldflags settings and in
+// their absence fallback to the settings in pkg/version/version.go
+func Get() apimachineryversion.Info {
+	return *version
+}
+
+func init() {
+	var (
+		versionParts = strings.Split(gitVersion, ".")
+		gitMajor     string
+		gitMinor     string
+	)
+
+	if len(versionParts) >= 2 {
+		gitMajor = strings.TrimPrefix(versionParts[0], "v")
+		gitMinor = versionParts[1]
+	}
+
+	version = &apimachineryversion.Info{
+		Major:        gitMajor,
+		Minor:        gitMinor,
+		GitVersion:   gitVersion,
+		GitCommit:    gitCommit,
+		GitTreeState: gitTreeState,
+		BuildDate:    buildDate,
+		GoVersion:    runtime.Version(),
+		Compiler:     runtime.Compiler,
+		Platform:     fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+	}
+}
