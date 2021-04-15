096a0a68717f2493cf42cf4f24939a8db58bc5b9
Sebastian Stauch <sebastian.stauch@sap.com>
2021-04-15 11:35:44 +0200

    Add istio scheme to gardener resource manager for reversed vpn setup.
    Co-authored-by: Johannes Scheerer <johannes.scheerer@sap.com>

--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/spf13/pflag v1.0.5
 	go.uber.org/zap v1.15.0
 	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e
+	istio.io/client-go v1.8.1
 	k8s.io/api v0.19.6
 	k8s.io/apiextensions-apiserver v0.19.6
 	k8s.io/apimachinery v0.19.6
--- pkg/cmd/target.go
+++ pkg/cmd/target.go
@@ -26,6 +26,7 @@ import (
 	volumesnapshotv1beta1 "github.com/kubernetes-csi/external-snapshotter/v2/pkg/apis/volumesnapshot/v1beta1"
 	"github.com/spf13/pflag"
 	"golang.org/x/time/rate"
+	networkingistiov1beta1 "istio.io/client-go/pkg/apis/networking/v1beta1"
 	corev1 "k8s.io/api/core/v1"
 	eventsv1 "k8s.io/api/events/v1"
 	eventsv1beta1 "k8s.io/api/events/v1beta1"
@@ -147,6 +148,7 @@ func getTargetScheme() *runtime.Scheme {
 	apiregistrationinstall.Install(scheme)
 	utilruntime.Must(hvpav1alpha1.AddToScheme(scheme))
 	utilruntime.Must(volumesnapshotv1beta1.AddToScheme(scheme))
+	utilruntime.Must(networkingistiov1beta1.AddToScheme(scheme))
 
 	return scheme
 }
