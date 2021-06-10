0cc705b369de337e7d58fa10a68d2c97446282f3
harishmanasa <79962951+harishmanasa@users.noreply.github.com>
2021-06-10 17:55:04 +0530

    made the import statement more readable
    Co-authored-by: Tim Ebert <timebertt@gmail.com>

--- pkg/controller/managedresource/merger.go
+++ pkg/controller/managedresource/merger.go
@@ -15,7 +15,7 @@
 package managedresource
 
 import (
-	v1alpha1 "github.com/gardener/gardener-resource-manager/api/resources/v1alpha1"
+	resourcesv1alpha1 "github.com/gardener/gardener-resource-manager/api/resources/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
